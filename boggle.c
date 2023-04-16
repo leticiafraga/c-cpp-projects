f04419db4e6ebf61cf824d36d9e626329101ae21
Leticia <leticiafraga55@gmail.com>
2023-04-16 12:41:28 -0300

    search right

--- boggle.c
+++ boggle.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void printGrid(int size, char *lines);
+int search(int position, int size, char *grid, int wordPosition, char *word);
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +17,7 @@ int main(int argc, char *argv[])
     }
 
     char *grid;
-    char lines[size][size];
+
     for (int i = 1; i < argc; i++)
     {
         if (i + 1 != argc)
@@ -26,22 +30,54 @@ int main(int argc, char *argv[])
             }
         }
     }
-    printf("The argument supplied is %s\n", grid);
+
+    printGrid(size, grid);
+
+    char *word = malloc(size);
+    int a;
+    do
+    {
+        scanf("%s", word);
+
+        int len = strlen(word);
+
+        for (int i = 0; i < size * size; i++)
+        {
+
+            if (*(grid + i) == word[0])
+            {
+
+                int found = search(i, size, grid, 1, word);
+                if (found != 0)
+                {
+                    printf("%d\n", found);
+                }
+            }
+        }
+
+    } while (word[0] != '0');
+
+    free(word);
+
+    return (0);
+}
+
+void printGrid(int size, char *lines)
+{
 
     for (int i = 0; i < (size * 2 + 3); i++)
     {
         printf("+");
     }
-     printf("\n");
+    printf("\n");
 
     for (int i = 0; i < size; i++)
     {
-        strncpy(lines[i], grid + (i * size), size);
-
-        printf("| ");     
+        printf("| ");
         for (int j = 0; j < size; j++)
         {
-            printf("%c ", lines[i][j]);
+
+            printf("%c ", *(lines + (i * size + j)));
         }
         printf("|\n");
     }
@@ -51,6 +87,24 @@ int main(int argc, char *argv[])
         printf("+");
     }
     printf("\n");
+}
 
-    return (0);
+int search(int position, int size, char *grid, int wordPosition, char *word)
+{
+    int len = strlen(word);
+    int found = 0;
+    int maxPosition = size * size;
+
+    if (wordPosition == len)
+        return 1;
+
+    // search right
+    if (position % size != (size - 1) && *(grid + position + 1) == word[wordPosition])
+    {
+        found = search(position + 1, size, grid, wordPosition + 1, word);
+        if (found)
+            return found;
+    }
+
+    return found;
 }
